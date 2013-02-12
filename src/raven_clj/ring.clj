97e811e66cde5d6083e3903be7d2b13be5dd42e5
Seth Buntin <seth.buntin@gmail.com>
2013-02-11 22:43:54 -0600

    add ability to send more attribute information along with the wrap-sentry function

--- src/raven_clj/ring.clj
+++ src/raven_clj/ring.clj
@@ -2,12 +2,13 @@
   (:require [raven-clj.core :refer [notify]]
             [raven-clj.interfaces :refer [http stacktrace]]))
 
-(defn wrap-sentry [handler config]
+(defn wrap-sentry [handler dsn & [extra]]
   (fn [req]
     (try
       (handler req)
       (catch Exception e
-        (future (notify config (-> {:message (.getMessage e)}
-                                   (http req)
-                                   (stacktrace e))))
+        (future (notify dsn (-> (merge extra
+                                       {:message (.getMessage e)})
+                                (http req)
+                                (stacktrace e))))
         (throw e)))))
