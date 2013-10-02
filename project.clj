4a1a49aee3f2fa82f51cda7c655cc19c77f4be94
Seth Buntin <seth.buntin@gmail.com>
2013-10-02 12:13:50 -0500

    bump version to 0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject raven-clj "0.7.0"
+(defproject raven-clj "0.8.0"
   :description "Sentry clojure client"
   :url "http://github.com/sethtrain/raven-clj"
   :license {:name "Eclipse Public License"
