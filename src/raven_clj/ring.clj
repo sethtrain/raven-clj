bd4c0d1365194b95f9eef166987beebcd9062d84
Seth Buntin <seth.buntin@gmail.com>
2013-12-05 14:52:41 -0600

    add catch clause for AssertionError

--- src/raven_clj/ring.clj
+++ src/raven_clj/ring.clj
@@ -2,13 +2,19 @@
   (:require [raven-clj.core :refer [capture]]
             [raven-clj.interfaces :refer [http stacktrace]]))
 
+(defn capture-error [dsn req extra ^Throwable error]
+  (future (capture dsn (-> (merge extra
+                                  {:message (.getMessage error)})
+                           (http req)
+                           (stacktrace error)))))
+
 (defn wrap-sentry [handler dsn & [extra]]
   (fn [req]
     (try
       (handler req)
       (catch Exception e
-        (future (capture dsn (-> (merge extra
-                                        {:message (.getMessage e)})
-                                 (http req)
-                                 (stacktrace e))))
+        (capture-error dsn req extra e)
+        (throw e))
+      (catch AssertionError e
+        (capture-error dsn req extra e)
         (throw e)))))
